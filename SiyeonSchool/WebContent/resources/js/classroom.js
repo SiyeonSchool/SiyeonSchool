f24572f0f13e16bd9a54e89fe63fc388f8ffa85f
John Kim <john.donggyu.kim@gmail.com>
2024-08-23 23:37:39 +0900

    도구 메인페이지 화면구현 완료

--- SiyeonSchool/WebContent/resources/js/classroom.js
+++ SiyeonSchool/WebContent/resources/js/classroom.js
@@ -1,7 +1,1 @@
-/* ====================  JSP에서 가져온 정보 ==================== */
-// 로그인유저정보
-const loginUser = JSON.parse(loginUserJson); // json string -> object 타입으로 형변환함.
-
-console.log(loginUser);
-console.log("currentBoardNo:" + currentBoardNo);
-
+console.log("수업 게시판 js 실행됨");
